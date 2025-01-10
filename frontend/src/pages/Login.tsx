80afc46ed433166f81d55ca02a4c12286dcf2061
JithinGeorge10 <jithin.george.el@gmail.com>
2025-01-10 10:19:34 +0530

    changed login page to responsive

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { app } from "../firebase/config";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Login: React.FC = () => {
-    const handleGoogleLogin = async() => {
+    const handleGoogleLogin = async () => {
         const auth = getAuth(app);
         const provider = new GoogleAuthProvider();
         provider.setCustomParameters({ prompt: 'select_account' });
@@ -12,23 +12,25 @@ const Login: React.FC = () => {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
             console.log("User Info:", user);
-            
         } catch (error) {
             console.log(error);
         }
+    };
 
-    }
     return (
         <>
-            <div className="bg-pink-50 font-sans min-h-screen flex flex-col items-center justify-center px-6 md:px-12">
+            <div className="bg-pink-50 font-sans min-h-screen flex items-center justify-center px-6">
                 <div className="flex flex-col md:flex-row items-center w-full max-w-7xl gap-12">
-                    <div className="text-center md:text-left md:w-1/2 space-y-6">
+                    <div className="text-center md:text-left md:w-1/2 space-y-6 flex flex-col items-center md:items-start">
                         <h1 className="text-purple-700 text-3xl font-bold">TaskBuddy</h1>
                         <p className="text-gray-600 text-lg">
                             Streamline your workflow and track progress effortlessly with our
                             all-in-one task management app.
                         </p>
-                        <button onClick={handleGoogleLogin} className="bg-black text-white px-6 py-3 rounded-full flex items-center justify-center gap-2 hover:bg-gray-800">
+                        <button 
+                            onClick={handleGoogleLogin} 
+                            className="bg-black text-white px-6 py-3 rounded-full flex items-center justify-center gap-2 hover:bg-gray-800"
+                        >
                             <img
                                 src="./google logo.png"
                                 alt="Google Icon"
@@ -37,14 +39,13 @@ const Login: React.FC = () => {
                             <span>Continue with Google</span>
                         </button>
                     </div>
-
-                    <img
-                        src="/task list view.png"
-                        alt="TaskBuddy Illustration"
-                        className="relative z-10 w-full max-w-xl rounded-lg shadow-lg "
-
-                    />
-
+                    <div className="relative w-full md:w-1/2 flex justify-center overflow-hidden hidden md:block">
+                        <img
+                            src="/task list view.png"
+                            alt="TaskBuddy Illustration"
+                            className="relative z-10 w-full max-w-xl rounded-lg shadow-lg"
+                        />
+                    </div>
                 </div>
             </div>
         </>
