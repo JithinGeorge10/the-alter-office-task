aed8cf71029b05b67b108900541b77924988e83c
JithinGeorge10 <jithin.george.el@gmail.com>
2025-01-26 18:53:40 +0530

    removed unsed usenavigte

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import {
   FaThLarge, FaSearch, FaListUl,
